aa1df901e6d3777e4c4aa22333ba62a7829fdcfc
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-02-18 15:17:14 -0600

    hero section

--- src/components/Landing/Hero/Hero.js
+++ src/components/Landing/Hero/Hero.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
 import data from "../../../data/img";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./Hero.css";
 
 export default function Hero() {
   return (
-    <div className="hero">
-      <>
-        {data.map(({ id, url, title, desc }) => {
-          return (
-            <div className="hero-title-section">
-              <img src={url} alt={title} />
+    // <div className="hero">
+    <Carousel
+      autoPlay
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+    >
+      {data.map(({ id, url, title, desc }) => {
+        return (
+          <div className="hero-title-section">
+            <img className="hero-image" src={url} alt={title} />
+            <div className="hero-text-section">
               <p className="hero-title">{title}</p>
               <p className="hero-desc">{desc}</p>
             </div>
-          );
-        })}
-      </>
-    </div>
+          </div>
+        );
+      })}
+    </Carousel>
+    // </div>
   );
 }
--- src/data/img.js
+++ src/data/img.js
@@ -7,13 +7,13 @@ const img = [
   },
   {
     id: "2",
-    url: "/img/kiran/Fall1.png",
+    url: "/img/kiran/lab.jpg",
     title: "Testing 2",
     desc: "Excepteur nisi id anim cillum do esse exercitation id proident. Velit ullamco aliquip labore magna sint laborum ea ipsum. Reprehenderit sint dolore consequat nulla aliquip officia exercitation consectetur incididunt dolor amet cillum adipisicing.",
   },
   {
     id: "3",
-    url: "/img/kiran/Flood1.png",
+    url: "/img/kiran/plant.jpg",
     title: "Testing 3",
     desc: "Excepteur nisi id anim cillum do esse exercitation id proident. Velit ullamco aliquip labore magna sint laborum ea ipsum. Reprehenderit sint dolore consequat nulla aliquip officia exercitation consectetur incididunt dolor amet cillum adipisicing.",
   },
