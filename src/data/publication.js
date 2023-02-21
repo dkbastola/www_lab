a47ec42e0d5458d8c29eb211ba0e8cc5c09c9462
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-02-21 17:41:26 -0600

    publication page

---
+++ src/data/publication.js
@@ -0,0 +1,34 @@
+const publication = [
+  {
+    id: 1,
+    title:
+      "Comprehension and data sharing behavior of Direct-To-Consumer genetic test customers (2016)",
+    desc: "The aim of this study was to evaluate current direct-to-consumer (DTC) genetic customers' ability to interpret and comprehend test results and to determine if honest brokers are needed. One hundred and twenty-two customers of the DTC genetic testing company 23andMe were polled in an online survey. Most of the subjects were able to correctly interpret the test cases, yet a majority did not share their results with a health-care professional. As the market for DTC genetic testing grows, test comprehension will become more critical. Involving more health professionals in this process may be necessary to ensure proper interpretations.",
+  },
+  {
+    id: 2,
+    title:
+      "Identifying enriched drug fragments as possible candidates for metabolic engineering (2016).",
+    desc: "Fragment-based approaches have now become an important component of the drug discovery process. In this study we introduce and discuss a computational pipeline to automatically extract statistically overrepresented chemical fragments in therapeutic classes, and search for similar fragments in a large database of natural products. By systematically identifying enriched fragments in therapeutic groups, we are able to extract and focus on few fragments that are likely to be active or structurally important. We show that several therapeutic classes (including antibacterial, antineoplastic, and drugs active on the cardiovascular system, among others) have enriched fragments that are also found in many natural compounds.",
+  },
+  {
+    id: 3,
+    title:
+      "Contribution of bioinformatics prediction in microRNA-based cancer therapeutics (2015)",
+    desc: "MicroRNAs (miRNAs) are small non-coding RNAs that serve to regulate gene expression. The discovery of deregulated miRNAs in cancer cells and tissues has led many to investigate the use of miRNAs as potential biomarkers for early detection, and as a therapeutic agent to treat cancer. Here we describe advancements in computational approaches to predict miRNAs and their targets, and discuss the role of bioinformatics in studying miRNAs in the context of human cancer.",
+  },
+  {
+    id: 4,
+    title: "Publications from ACM and Affiliated Organizations",
+    desc: "",
+    link: "https://dl.acm.org/profile/99659404391",
+  },
+  {
+    id: 5,
+    title: "Publications listed in pubmed",
+    desc: "",
+    link: "http://tinyurl.com/bastola-bio",
+  },
+];
+
+export default publication;
--- src/pages/Publication/Publication.js
+++ src/pages/Publication/Publication.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import publication from "../../data/publication";
 import "./Publication.css";
 
 export default function Publication() {
-  return <div className="publication">Publication page</div>;
+  return (
+    <div className="publication">
+      <div className="publication-header">
+        <div className="publication-header-heading">
+          Welcome to My Publication Page!
+        </div>
+      </div>
+      <div className="publication-list-section">
+        <ul className="publication-list">
+          {publication.map(({ id, title, desc, link }) => {
+            return (
+              <li key={id} className="publication-listitem">
+                {link ? (
+                  <Link to={link}>
+                    <h3 className="publication-topic">{title}</h3>
+                  </Link>
+                ) : (
+                  <h3 className="publication-topic">{title}</h3>
+                )}
+                <p className="publication-desc">{desc}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
