ddb0c5a9c970b5182f0a91c391a81c817d76e200
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-02-23 12:29:17 -0600

    research page - first commit

---
+++ src/data/research.js
@@ -0,0 +1,51 @@
+const research = [
+  {
+    id: 1,
+    title: "C-LAB ( Collaborative Laboratory for Applied Bioinformatics)",
+    desc: "We have witnessed tremendous successes in world food production over the past five decades. This has made food more affordable for the majority of the world's population, despite a doubling in population. However, it has exerted undesirable pressure on terrestrial as well as aquatic ecosystems and they are now failing to provide adequate nutrition. This model of resource-intensive agriculture is not desirable in a world with finite resources provided by the Earthâ€™s land, oceans, and atmosphere. Therefore, we have recently built several Food computers in our lab, which is expected to contribute in the development of a new food production model that is environmentally friendly and takes advantage of the advancement in technology and the Omics-knowledge relating to living organism to make it economically desirable as well. Two major research areas of interest in the Collaborative Laboratory for Applied Bioinformatics (CLAB) include: (a) how the function of mitochondria, the major contributor of cell metabolism impact not only the quality of food produced in plant but also affect human health and various diseases including cancer, neurodegeneration and microbiome. (b) What are the technological choices and paths for improving productivity, economic and environmental performance of culinary herbs and medicinally important plants?",
+  },
+  {
+    id: 2,
+    title: "BioCore",
+    desc: "As a Bioinformatics Core we collaborate with researchers from differnt educational/research institutes. These research encompasses the study of protein evolution-structure-function relationships, biological text mining, proteomic bioinformatics, computational systems biology, and bioinformatics cyberinfrastructure.",
+  },
+  {
+    id: 3,
+    title: "BioCMS a web-portal",
+    desc: "https://biocms.ist.unomaha.edu/",
+    link: "https://biocms.ist.unomaha.edu/",
+  },
+  {
+    id: 4,
+    title: "Mass spectorscopy data analysis pipeline with open source tools",
+    desc: "",
+  },
+  {
+    id: 5,
+    title: "Requeste tracker: A task managment system",
+    desc: "http://biodoc.ist.unomaha.edu/rt",
+    link: "http://biodoc.ist.unomaha.edu/rt",
+  },
+  {
+    id: 6,
+    title: "Advnaced Molecular Identification System",
+    desc: "http://i3bio.ist.unomaha.edu/db/",
+    link: "http://i3bio.ist.unomaha.edu/db/",
+  },
+  {
+    id: 7,
+    title:
+      "Classification of Fungal Sequences Using Characteristic Restriction Endonuclease Cut Order",
+    desc: "https://biobase.ist.unomaha.edu/projects/creco/",
+    link: "https://biobase.ist.unomaha.edu/projects/creco/",
+  },
+  {
+    id: 8,
+    title:
+      "Information Technology Solution for the Assembly, Annotation and Analysis of Nucleotide Sequence Data",
+    desc: "https://hivis.ist.unomaha.edu/",
+    link: "https://hivis.ist.unomaha.edu/",
+  },
+];
+
+export default research;
--- src/pages/Research/Research.js
+++ src/pages/Research/Research.js
@@ -1,7 +1,66 @@
+import { Paper } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
+import research from "../../data/research";
 import "./Research.css";
 
 export default function Research() {
-  return <div className="research">Research page</div>;
+  return (
+    <div className="research">
+      <div className="research-header">
+        <div className="research-header-heading">
+          <div className="research-title">
+            <h1>My Research</h1>
+            <p>
+              So you are interested in my research? Check out the most recent
+              research down below!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="research-list-section">
+        <h3 className="research-list-header">Recent Research</h3>
+        <p className="research-list-desc">
+          The use of computing to manage, organize and analyze biological and
+          clinical data has become very important element of biology and medical
+          research. My current research in bioinformatics is rooted in an
+          interdisciplinary content both in style and context due to my decade
+          long professional career in a molecular biology laboratory. I bring my
+          wide variety of research experience in the life sciences (plant,
+          animal bacteria and virus molecular biology and biochemistry) into the
+          classroom and into my research work in bioinformatics. One of the
+          broad goals of my research work in biomedical informatics encompasses
+          the study of information transmission and exchange in living systems,
+          with a particular interest in mitochondria. Additionally, my
+          collaborative translational research effort includes the study of
+          effective communication methods between health service providers and
+          patients with chronic diseases, such as diabetes, COPD and
+          hypertension (common among the elderly population). We are currently
+          evaluating the role of an information technology solution (telehealth)
+          in the delivery of remote health services to further health care
+          services to rural areas where traditional medicine is largely
+          inaccessible.
+        </p>
+        <ul className="research-list">
+          {research.map(({ id, title, desc, link }) => {
+            return (
+              <li key={id} className="research-listitem">
+                <Paper elevation={3} className="research-paper">
+                  {link ? (
+                    <Link to={link}>
+                      <h3 className="research-topic">{title}</h3>
+                    </Link>
+                  ) : (
+                    <h3 className="research-topic">{title}</h3>
+                  )}
+                  <p className="research-desc">{desc}</p>
+                </Paper>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
