2955db2bb582e36f6c77bc982b4a94eb6d25a36c
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-02-24 09:35:32 -0600

    research section

--- src/pages/Research/Research.js
+++ src/pages/Research/Research.js
@@ -21,27 +21,32 @@ export default function Research() {
       </div>
       <div className="research-list-section">
         <h3 className="research-list-header">Recent Research</h3>
-        <p className="research-list-desc">
-          The use of computing to manage, organize and analyze biological and
-          clinical data has become very important element of biology and medical
-          research. My current research in bioinformatics is rooted in an
-          interdisciplinary content both in style and context due to my decade
-          long professional career in a molecular biology laboratory. I bring my
-          wide variety of research experience in the life sciences (plant,
-          animal bacteria and virus molecular biology and biochemistry) into the
-          classroom and into my research work in bioinformatics. One of the
-          broad goals of my research work in biomedical informatics encompasses
-          the study of information transmission and exchange in living systems,
-          with a particular interest in mitochondria. Additionally, my
-          collaborative translational research effort includes the study of
-          effective communication methods between health service providers and
-          patients with chronic diseases, such as diabetes, COPD and
-          hypertension (common among the elderly population). We are currently
-          evaluating the role of an information technology solution (telehealth)
-          in the delivery of remote health services to further health care
-          services to rural areas where traditional medicine is largely
-          inaccessible.
-        </p>
+        <div className="research-list-desc">
+          <p>
+            The use of computing to manage, organize and analyze biological and
+            clinical data has become very important element of biology and
+            medical research. My current research in bioinformatics is rooted in
+            an interdisciplinary content both in style and context due to my
+            decade long professional career in a molecular biology laboratory. I
+            bring my wide variety of research experience in the life sciences
+            (plant, animal bacteria and virus molecular biology and
+            biochemistry) into the classroom and into my research work in
+            bioinformatics.
+          </p>
+          <p>
+            One of the broad goals of my research work in biomedical informatics
+            encompasses the study of information transmission and exchange in
+            living systems, with a particular interest in mitochondria.
+            Additionally, my collaborative translational research effort
+            includes the study of effective communication methods between health
+            service providers and patients with chronic diseases, such as
+            diabetes, COPD and hypertension (common among the elderly
+            population). We are currently evaluating the role of an information
+            technology solution (telehealth) in the delivery of remote health
+            services to further health care services to rural areas where
+            traditional medicine is largely inaccessible.
+          </p>
+        </div>
         <ul className="research-list">
           {research.map(({ id, title, desc, link }) => {
             return (
