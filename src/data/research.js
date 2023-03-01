e38bf07fb2c399c674441cbcad5ddaaad5755870
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-03-01 16:34:46 -0600

    change materials in research api

--- src/data/research.js
+++ src/data/research.js
@@ -1,50 +1,19 @@
 const research = [
   {
     id: 1,
-    title: "C-LAB ( Collaborative Laboratory for Applied Bioinformatics)",
-    desc: "We have witnessed tremendous successes in world food production over the past five decades. This has made food more affordable for the majority of the world's population, despite a doubling in population. However, it has exerted undesirable pressure on terrestrial as well as aquatic ecosystems and they are now failing to provide adequate nutrition. This model of resource-intensive agriculture is not desirable in a world with finite resources provided by the Earthâ€™s land, oceans, and atmosphere. Therefore, we have recently built several Food computers in our lab, which is expected to contribute in the development of a new food production model that is environmentally friendly and takes advantage of the advancement in technology and the Omics-knowledge relating to living organism to make it economically desirable as well. Two major research areas of interest in the Collaborative Laboratory for Applied Bioinformatics (CLAB) include: (a) how the function of mitochondria, the major contributor of cell metabolism impact not only the quality of food produced in plant but also affect human health and various diseases including cancer, neurodegeneration and microbiome. (b) What are the technological choices and paths for improving productivity, economic and environmental performance of culinary herbs and medicinally important plants?",
+    title: "Bioinformatics and Computational Biology",
+    desc: "Our research group uses biosystems, computational biology, and bioinformatics to advance our understanding of colorectal cancer (CRC) and identify biomarkers for diagnosis and treatment. Specifically, our studies have identified potential biomarkers for detecting CRC, investigated the role of claudins in CRC, proposed a Data-Driven Reference approach for identifying reliable biomarkers, investigated the role of claudins in obesity-induced organ and tissue-specific tight junction restructuring, and conducted a coexpression network analysis of miRNA-142 overexpression in neuronal cells. These studies have significantly contributed to our understanding of CRC and related diseases, as well as the development of reliable biomarkers for diagnosis and treatment.",
   },
   {
     id: 2,
-    title: "BioCore",
-    desc: "As a Bioinformatics Core we collaborate with researchers from differnt educational/research institutes. These research encompasses the study of protein evolution-structure-function relationships, biological text mining, proteomic bioinformatics, computational systems biology, and bioinformatics cyberinfrastructure.",
+    title: "Health Informatics and Public Health",
+    desc: "Our research group applies information technology to healthcare and conducts studies in health informatics. We use various data-driven and technological approaches to improve healthcare outcomes. Our studies include investigating glycemic variability and diabetes complications, identifying patient characteristics for better glycemic control, summarizing international workshops on technology and data-driven innovations in cancer care, evaluating medical professionals' interpretation of genetic test results and opinions on direct-to-consumer genetic testing, analyzing cancer patients' and healthcare providers' perceptions of chemotherapy using Twitter data, and developing predictive models of mosquito-borne disease spread using social media and other open intelligence sources. These studies exemplify the potential of health informatics to improve healthcare outcomes and tackle public health challenges. They represent a small sample of the diverse research that falls under the health informatics umbrella. ",
   },
   {
     id: 3,
-    title: "BioCMS a web-portal",
-    desc: "https://biocms.ist.unomaha.edu/",
+    title: "Applied Informatics in Pharmacology and Pharmacogenomics ",
+    desc: "Our work covers a wide range of fields, including biosystems and applied informatics in pharmacology and pharmacogenomics. We use computational methods to investigate the molecular mechanisms underlying biological processes, such as metabolite production, cancer therapy, drug discovery, and protein localization. We've developed the HerbMicrobeDataBase (HMDB) to study the effects of culinary herbs on gut health and have studied the role of the host immune system in response to cancer therapy. Our group has also developed computational pipelines for predicting therapeutic activities of natural compounds and identifying proteins localized to mitochondria. Through our work, we aim to improve our understanding of various biological processes and contribute to drug discovery. ",
     link: "https://biocms.ist.unomaha.edu/",
-  },
-  {
-    id: 4,
-    title: "Mass spectorscopy data analysis pipeline with open source tools",
-    desc: "",
-  },
-  {
-    id: 5,
-    title: "Requeste tracker: A task managment system",
-    desc: "http://biodoc.ist.unomaha.edu/rt",
-    link: "http://biodoc.ist.unomaha.edu/rt",
-  },
-  {
-    id: 6,
-    title: "Advnaced Molecular Identification System",
-    desc: "http://i3bio.ist.unomaha.edu/db/",
-    link: "http://i3bio.ist.unomaha.edu/db/",
-  },
-  {
-    id: 7,
-    title:
-      "Classification of Fungal Sequences Using Characteristic Restriction Endonuclease Cut Order",
-    desc: "https://biobase.ist.unomaha.edu/projects/creco/",
-    link: "https://biobase.ist.unomaha.edu/projects/creco/",
-  },
-  {
-    id: 8,
-    title:
-      "Information Technology Solution for the Assembly, Annotation and Analysis of Nucleotide Sequence Data",
-    desc: "https://hivis.ist.unomaha.edu/",
-    link: "https://hivis.ist.unomaha.edu/",
   },
 ];
 
