e73cddcc766dd765ee263bf13a485bdd6c185bc9
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-03-03 09:06:39 -0600

    add citations

--- src/data/research.js
+++ src/data/research.js
@@ -3,17 +3,71 @@ const research = [
     id: 1,
     title: "Bioinformatics and Computational Biology",
     desc: "Our research group uses biosystems, computational biology, and bioinformatics to advance our understanding of colorectal cancer (CRC) and identify biomarkers for diagnosis and treatment. Specifically, our studies have identified potential biomarkers for detecting CRC, investigated the role of claudins in CRC, proposed a Data-Driven Reference approach for identifying reliable biomarkers, investigated the role of claudins in obesity-induced organ and tissue-specific tight junction restructuring, and conducted a coexpression network analysis of miRNA-142 overexpression in neuronal cells. These studies have significantly contributed to our understanding of CRC and related diseases, as well as the development of reliable biomarkers for diagnosis and treatment.",
+    citations: [
+      {
+        desc: "Goettsch, K. A., Zhang, L., Singh, A. B., Dhawan, P., & Bastola, D. K. (2022). Reliable epithelial–mesenchymal transition biomarkers for colorectal cancer detection. Biomarkers in Medicine, 16(12), 889-901.  ",
+      },
+      {
+        desc: "Gowrikumar, S., Primeaux, M., Pravoverov, K., Wu, C., Szeglin, B. C., Sauvé, C. E. G., ... & Dhawan, P. (2021). A claudin-based molecular signature identifies high-risk, chemoresistant colorectal cancer patients. Cells, 10(9), 2211. ",
+      },
+      {
+        desc: "Zhang, L., Thapa, I., Haas, C., & Bastola, D. (2019). Multiplatform biomarker identification using a data-driven approach enables single-sample classification. BMC bioinformatics, 20, 1-13. ",
+      },
+      {
+        desc: "Ahmad, R., Rah, B., Bastola, D., Dhawan, P., & Singh, A. B. (2017). Obesity-induces organ and tissue specific tight junction restructuring and barrier deregulation by claudin switching. Scientific reports, 7(1), 5125. ",
+      },
+      {
+        desc: "Thapa, I., Fox, H. S., & Bastola, D. (2015). Coexpression network analysis of miRNA-142 overexpression in neuronal cells. BioMed research international, 2015 ",
+      },
+    ],
   },
   {
     id: 2,
     title: "Health Informatics and Public Health",
     desc: "Our research group applies information technology to healthcare and conducts studies in health informatics. We use various data-driven and technological approaches to improve healthcare outcomes. Our studies include investigating glycemic variability and diabetes complications, identifying patient characteristics for better glycemic control, summarizing international workshops on technology and data-driven innovations in cancer care, evaluating medical professionals' interpretation of genetic test results and opinions on direct-to-consumer genetic testing, analyzing cancer patients' and healthcare providers' perceptions of chemotherapy using Twitter data, and developing predictive models of mosquito-borne disease spread using social media and other open intelligence sources. These studies exemplify the potential of health informatics to improve healthcare outcomes and tackle public health challenges. They represent a small sample of the diverse research that falls under the health informatics umbrella. ",
+    citations: [
+      {
+        desc: "Nwadiugwu, M. C., Bastola, D. R., Haas, C., & Russell, D. (2021). Identifying glycemic variability in diabetes patient cohorts and evaluating disease outcomes. Journal of clinical medicine, 10(7), 1477. ",
+      },
+      {
+        desc: "Kondylakis, H., Axenie, C., Bastola, D., Katehakis, D. G., Kouroubali, A., Kurz, D., ... & Zwiggelaar, R. (2020). Status and recommendations of technological and data-driven innovations in cancer care: Focus group study. Journal of medical Internet research, 22(12), e22034. ",
+      },
+      {
+        desc: "McGrath, S. P., Walton, N., Williams, M. S., Kim, K. K., & Bastola, K. (2019). Are providers prepared for genomic medicine: interpretation of Direct-to-Consumer genetic testing (DTC-GT) results and genetic self-efficacy by medical professionals. BMC health services research, 19(1), 1-12. ",
+      },
+      {
+        desc: "Zhang, L., Hall, M., & Bastola, D. (2018). Utilizing Twitter data for analysis of chemotherapy. International journal of medical informatics, 120, 92-100. ",
+      },
+      {
+        desc: "Cooper, K. M., Bastola, D. R., Gandhi, R., Ghersi, D., Hinrichs, S., Morien, M., & Fruhling, A. (2016). Forecasting the spread of mosquito-borne disease using publicly accessible data: a case study in Chikungunya. In AMIA Annual Symposium Proceedings (Vol. 2016, p. 431). American Medical Informatics Association. ",
+      },
+      {
+        desc: "McGrath, S. P., Coleman, J., Najjar, L., Fruhling, A., & Bastola, D. R. (2016). Comprehension and data-sharing behavior of direct-to-consumer genetic test customers. Public Health Genomics, 19(2), 116-124. ",
+      },
+      {
+        desc: "Campbell, W. S., Pedersen, J., McClay, J. C., Rao, P., Bastola, D., & Campbell, J. R. (2015). An alternative database approach for management of SNOMED CT and improved patient data queries. Journal of biomedical informatics, 57, 350-357. ",
+      },
+    ],
   },
   {
     id: 3,
     title: "Applied Informatics in Pharmacology and Pharmacogenomics ",
     desc: "Our work covers a wide range of fields, including biosystems and applied informatics in pharmacology and pharmacogenomics. We use computational methods to investigate the molecular mechanisms underlying biological processes, such as metabolite production, cancer therapy, drug discovery, and protein localization. We've developed the HerbMicrobeDataBase (HMDB) to study the effects of culinary herbs on gut health and have studied the role of the host immune system in response to cancer therapy. Our group has also developed computational pipelines for predicting therapeutic activities of natural compounds and identifying proteins localized to mitochondria. Through our work, we aim to improve our understanding of various biological processes and contribute to drug discovery. ",
     link: "https://biocms.ist.unomaha.edu/",
+    citations: [
+      {
+        desc: "Chandrababu, S., & Bastola, D. (2019). An integrated approach to recognize potential protective effects of culinary herbs against chronic diseases. Journal of Healthcare Informatics Research, 3, 184-199. ",
+      },
+      {
+        desc: "Karri, K., & Bastola, D. R. (2017). Efficacy of multiple anticancer therapies may depend on host immune response. Advances in Science, Technology and Engineering Systems, 2(3), 773-787. ",
+      },
+      {
+        desc: "Sharma, S., Karri, K., Thapa, I., Bastola, D., & Ghersi, D. (2016). Identifying enriched drug fragments as possible candidates for metabolic engineering. BMC medical genomics, 9(2), 167-177 ",
+      },
+      {
+        desc: "Banwait, J. K., Goettsch, K. A., & Bastola, D. R. (2016). Proteins localised to human mitochondria show structural element enrichment in their corresponding mRNA. International Journal of Computational Biology and Drug Design, 9(1-2), 120-134. ",
+      },
+    ],
   },
 ];
 
