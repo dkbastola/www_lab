1dce84c8c925fe9edee3571f35b4ff1cd7c4a573
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2023-03-07 09:30:21 -0600

    meet our team

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -3,5 +3,16 @@ import React from "react";
 import "./Members.css";
 
 export default function Members() {
-  return <div className="members">Members page</div>;
+  return (
+    <div className="members">
+      <div className="members-header">
+        <div className="members-header-heading">
+          <div className="members-title">
+            <h1>Meet Our Team</h1>
+            <p>Get to know the people behind our lab's success</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
